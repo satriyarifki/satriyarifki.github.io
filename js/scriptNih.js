121a0b607bf4ba5996160378590cc79aa0d1911a
satriyarp <rifkisatriya@gmail.com>
2021-09-03 12:27:10 +0700

    fix video js, add image

--- js/scriptNih.js
+++ js/scriptNih.js
@@ -1,32 +1,18 @@
-var myModal = document.querySelector('.modal');
+const myModal = document.querySelectorAll('.modal');
 const myInput = document.getElementsByClassName('.input');
-
+const vide = document.querySelectorAll('video');
 // myModal.addEventListener('shown.bs.modal', function () {
 //   myInput.focus();
 // });
 
 console.log(document);
 console.log(myModal);
-var vide = document.querySelector('video');
-var closo = document.querySelector('.clos');
-
-document.getElementById('myVideo');
-document.getElementsByClassName('.clos');
+console.log(vide);
 
 for (i = 0; i < myModal.length; i++) {
-  myModal[i].addEventListener('show', function () {
-    console.log('ok');
+  myModal[i].addEventListener('hidden.bs.modal', function () {
+    for (i = 0; i < myModal.length; i++) {
+      vide[i].pause();
+    }
   });
 }
-
-// clos.addEventListener('click', function () {
-//   console.log('close');
-// });
-
-document.getElementsByClassName('.pid').onplay = function () {
-  console.log('play');
-};
-
-$('.pid').onplay = function () {
-  console.log('play');
-};
